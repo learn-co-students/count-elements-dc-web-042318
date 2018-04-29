fb0bf2df18edab7be3954860ba96f1040a8b6a02
codingkasia <codingkasia@gmail.com>
2018-04-29 00:03:55 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
-def count_elements(array)
-  # code goes here
-end
- 
+require 'pry'
+
+  # array = ['cat', 'dog', 'fish', 'fish']
+  #   hash = count_elements(array)
+  #   expect(hash['cat']).to eq(1)
+  #   expect(hash['dog']).to eq(1)
+  #   expect(hash['fish']).to eq(2)
+
+  def count_elements(array)
+    new_hash = {}
+    array.map {|animal| new_hash.has_key?(animal) ? new_hash[animal] += 1 : new_hash[animal] = 1}
+    new_hash
+  end
